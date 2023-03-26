009f181f698417147d59e24391ac5a2ad15cb424
Yevhen Kashkarov <kea.riland@gmail.com>
2023-03-26 19:11:30 +0300

    add tests for price list by period

--- api/src/Service/PriceList/PriceListByPeriod.php
+++ api/src/Service/PriceList/PriceListByPeriod.php
@@ -5,6 +5,7 @@ namespace App\Service\PriceList;
 class PriceListByPeriod extends PriceList
 {
     const REQUIRED_FIELD = 'date';
+    const END_OF_DAY = 86399; //add 23:59:59
 
     public function __construct(private readonly PriceListInterface $priceList)
     {
@@ -21,11 +22,11 @@ class PriceListByPeriod extends PriceList
     private function checkPeriods(array $prices): array
     {
         $result = [];
-        $startDate = strtotime($this->pricesListDto->getStartDate()->format(DATE_ATOM));
-        $endDate = strtotime($this->pricesListDto->getEndDate()->format(DATE_ATOM));
+        $startDate = strtotime($this->pricesListDto->getStartDate()->format(DATE_ATOM)) + self::END_OF_DAY;
+        $endDate = strtotime($this->pricesListDto->getEndDate()->format(DATE_ATOM)) + self::END_OF_DAY;
 
         foreach ($prices as $price) {
-            if ($price[self::REQUIRED_FIELD] >= $startDate && $price[self::REQUIRED_FIELD] <= $endDate) {
+            if ($startDate >= $price[self::REQUIRED_FIELD] && $price[self::REQUIRED_FIELD] <= $endDate) {
                 $result[] = $price;
             }
         }
